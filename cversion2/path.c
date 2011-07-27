9c1f609264d14560a11105a21828760d73ee94fd
Marcio Diaz <marcio.diaz@gmail.com>
2011-07-27 13:52:54 -0300

    path module complete, with documentation

--- cversion2/path.c
+++ cversion2/path.c
@@ -1,40 +1,71 @@
 #include <assert.h>
 #include "path.h"
 
+/**
+ * Estructura para representar un nodo en el camino.
+ */
 
-struct snode{
-	uint balance:1;
+struct snode {
+	bool balance;
 	uint name:15;
 };
 
-struct path{
+
+/**
+ * Estructura que representa un camino con su flujo.
+ */
+
+struct path {
 	uint flow:31;
 	uint n_nodes:17;
 	snode *nodes;
 };
 
+/**
+ * Crea un nuevo camino.
+ * @returns El nuevo camino creado.
+ */
 
 path path_new() {
 	path path = NULL;
 
 	path = malloc(sizeof(struct path));
-	path->n_nodes=0;
+	path->n_nodes = 0;
 	path->nodes = NULL;
+	
+	assert(path != NULL);
 
 	return path;
 }
 
+/**
+ * Destruye un camino, liberando la memoria usada.
+ * @param p El camino a destruir.
+ */
+
 void path_destroy(path p){
 	uint i;
+
 	assert(p != NULL);
+
 	for (i = 0; i < p->n_nodes; i++)
 		free(p->nodes[i]);
+
 	free(p->nodes);
 	free(p);
-	p = NULL;
 }
 
-void path_add_node (path p, uint i, uint b) {
+/**
+ * Agrega un nodo al camino con balance @b.
+ * @param p El camino.
+ * @param i El ID del nodo a agregar.
+ * @param b Balance del nodo, true si es forward, 
+ * false si es backward.
+ */
+
+void path_add_node (path p, uint i, bool b) {
+	assert(p != NULL);
+	
 	p->nodes = realloc(p->nodes, 
 					   (p->n_nodes+1)
 					   * sizeof(snode));
@@ -44,22 +75,69 @@ void path_add_node (path p, uint i, uint b) {
 	p->n_nodes++;
 }
 
+/**
+ * Devuelve la longitud del camino.
+ * @param p El camino.
+ * @returns La longitud del camino.
+ */
+
 uint path_length(path p) {
+	assert(p != NULL);
+	
 	return p->n_nodes;
 }
 
+/**
+ * Devuelve el flujo que pasa a traves del camino.
+ * @param p El camino.
+ * @returns El flujo a traves del camino.
+ */
+
 uint path_flow(path p) {
+	assert(p != NULL);
+	
 	return p->flow;
 }
 
+/**
+ * Modifica el flujo del camino.
+ * @param p El camino.
+ * @param flow El nuevo valor del flujo del camino.
+ */
+
 void path_update_flow(path p, uint flow) {
+	assert(p != NULL);
+	
 	p->flow = flow;
 }
 
+/**
+ * Devuelve el ID del nodo @i-esimo en el camino.
+ * @param p El camino.
+ * @param i La posicion del nodo en el camino cuyo ID queremos obtener.
+ * @warning La longitud del camino debe ser mayor a @i.
+ * @returns El id del nodo en la posicion @i.
+ */
+
 uint path_nth_name(path p, uint i) {
+	assert(p != NULL);
+	assert(i < path_length(p));
+	
 	return p->nodes[i]->name;
 }
 
-uint path_nth_balance(path p, uint i) {
+/**
+ * Devuelve el balance del @i-esimo nodo del camino.
+ * @param p El camino.
+ * @param i La posicion del nodo.
+ * @warning La longitud del camino debe ser mayor a @i.
+ * @returns True si el @i-esimo nodo del camino es forward, False si
+ * el nodo es backward.
+ */
+
+bool path_nth_balance(path p, uint i) {
+	assert(p != NULL);
+	assert(i < path_length(p));
+
 	return p->nodes[i]->balance;
 }
--- cversion2/path.h
+++ cversion2/path.h
@@ -2,6 +2,7 @@
 #define _PATH_H_
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include "utils.h"
 
 typedef struct path * path;
@@ -9,11 +10,11 @@ typedef struct snode * snode;
 
 path path_new();
 void path_destroy(path p);
-void path_add_node (path p, uint i, uint b);
+void path_add_node (path p, uint i, bool b);
 uint path_length(path p);
 uint path_flow(path p);
 uint path_nth_name(path p, uint i);
-uint path_nth_balance(path p, uint i);
+bool path_nth_balance(path p, uint i);
 void path_update_flow(path p, uint flow);
 
 #endif
